1f419a7a24fe9396e30575c57eedf5b638162c8a
Shane Fuller <shnfllr17@gmail.com>
2020-04-13 21:00:10 -0400

    Change knapsack

--- Greedy_Two_Approximation.py
+++ Greedy_Two_Approximation.py
@@ -1,6 +1,11 @@
 from knapsack import Knapsack
 
+knapsack_instances = []
+
 for x in range(100):
     cx = Knapsack()
     cx.add_items()
-    print(str(cx))
+    knapsack_instances.append(cx)
+
+for t in knapsack_instances:
+    print(str(t))
